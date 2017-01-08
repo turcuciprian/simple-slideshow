4afb5d717f96c9960d9328e4f00be9910b52a676
Ciprian Turcu <turcuciprian1@gmail.com>
2017-01-08 16:04:52 +0200

    Added shortcode

--- simple-slideshow.php
+++ simple-slideshow.php
@@ -1,11 +1,27 @@
 <?php
 /*
-  Plugin Name: Simple Slideshow
+  Plugin Name: Simple Slideshow WPCluj
   Plugin URI: http://wordpress.org
   Description: A simple plugin for the cluj wordpress metup
   Version: 1.0
   Author: turcuciprian
   Author URI: http://wordpress.org
   License: GPLv2 or later
-  Text Domain: simple-slideshow
+  Text Domain: simple-slideshow-wpcluj
  */
+
+ // Register a new post type
+ add_action('init', 'ssRegisterSlides');
+ function ssRegisterSlides()
+ {
+     $args['label'] = 'Slides';
+     $args['public'] = true;
+     $args['show_ui'] = true;
+     register_post_type('slides', $args);
+ }
+ 
+ //Slideshow Shortcode
+ function ss_shortcode( $atts ){
+ 	return "Slideshow  code Goes here";
+ }
+ add_shortcode( 'ss-shortcode', 'ss_shortcode' );
