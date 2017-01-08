430d4a44fa3127b33994c7b3203934fcda2fa5f2
Ciprian Turcu <turcuciprian1@gmail.com>
2017-01-08 14:15:13 +0200

    added php file, made prlugin discoverable by wordpress, added appropriate naming and description

---
+++ simple-slideshow.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Simple Slideshow
+  Plugin URI: http://wordpress.org
+  Description: A simple plugin for the cluj wordpress metup
+  Version: 1.0
+  Author: turcuciprian
+  Author URI: http://wordpress.org
+  License: GPLv2 or later
+  Text Domain: simple-slideshow
+ */
